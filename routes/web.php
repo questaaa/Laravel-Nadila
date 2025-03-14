220ad4c130b3917aab1e4a621a04707a272db988
Nadila <n22148733@gmail.com>
2025-03-14 15:37:38 +0700

    Menambahkan halaman about, gallery, home dan menghapus welcome.blade.php

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,19 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+
+    return view('home');
+
+});
+
+Route::get('/About', function () {
+
+    return view('About');
+
+});
+
+Route::get('/Gallery', function () {
+
+    return view('Gallery');
+
 });
